e885b1ad8900c37a8bf25d2d41c357d8dec440e6
Paul Schaefer <yeppaul2@gmail.com>
2016-06-29 17:51:36 -0400

    Removed set colors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // Initiates Trianglify
 var pattern = Trianglify({
     height: document.getElementById('site_header').getClientRects()[0].height,
-    width:  document.getElementById('site_header').getClientRects()[0].width,
-    x_colors: 'Greys'
+    width:  document.getElementById('site_header').getClientRects()[0].width
 });
 document.getElementById('site_header').appendChild(pattern.canvas());
