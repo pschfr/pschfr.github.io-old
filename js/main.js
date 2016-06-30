6da6bf46b776811cc583c1f5db1dff84acba1ea8
Paul <yeppaul2@gmail.com>
2016-06-30 08:19:30 -0400

    tweaked colors, changed arrow animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Initiates Trianglify
 var pattern = Trianglify({
     height: document.getElementById('site_header').getClientRects()[0].height,
-    width:  document.getElementById('site_header').getClientRects()[0].width
+    width:  document.getElementById('site_header').getClientRects()[0].width,
+    x_colors: ['#aaa', '#aaa'],
+    y_colors: 'Purples'
 });
 document.getElementById('site_header').appendChild(pattern.canvas());
