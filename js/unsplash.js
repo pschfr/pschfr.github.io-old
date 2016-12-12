310688902839243d5a27ff00b542258c67f92775
Paul Schaefer <yeppaul2@gmail.com>
2016-12-11 22:33:42 -0500

    improved unsplash background filter, overlay

--- js/unsplash.js
+++ js/unsplash.js
@@ -1,12 +1,12 @@
 function randomBackground(time) { // daily, weekly, or every time
-	var categories = ['buildings', /*'food',*/ 'nature', /*'people', 'technology', 'objects'*/];
+	var categories = [/*'buildings', 'food',*/ 'nature', /*'people', 'technology', 'objects'*/];
 	var randomCategory = Math.floor((Math.random() * categories.length));
 	var photo = new UnsplashPhoto();
 
 	if (time == 'daily' || time == 'weekly')
-		photo.all().randomize(time).of('space').fetch();
+		photo.all().randomize(time).fromCategory(categories[randomCategory]).of('space').fetch();
 	else
-		photo.all().of('space').fetch();
+		photo.all().fromCategory(categories[randomCategory]).of('space').fetch();
 
 	document.getElementById('top').style.backgroundImage = "url(" + photo.url + ")";
 	document.getElementById('footer').style.backgroundImage = "url(" + photo.url + ")";
